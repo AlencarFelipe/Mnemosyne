fbb6377cc25a5ebd1109ba10a2d0899b1eeb6063
AlencarFelipe <alencarfelkipe@gmail.com>
2025-02-14 21:56:54 -0300

    Finalizando os exercicios

--- sandbox/condicionais.js
+++ sandbox/condicionais.js
@@ -1,87 +1,165 @@
-//Exercicio 1 
-let num = prompt("Digite um número:");
+// //Exercicio 1 
+// let num = prompt("Digite um número:");
 
-if(num>0){
+// if(num>0){
 
-    console.log("O numero é positivo");
+//     console.log("O numero é positivo");
 
-} else if (num < 0){
+// } else if (num < 0){
     
-    console.log("O numero é negativo");
+//     console.log("O numero é negativo");
 
-}else {
+// }else {
     
-   console.log("O numero é zero.")  
-}
+//    console.log("O numero é zero.")  
+// }
 
 
 
 
-//Exercicio 2
+// //Exercicio 2
 
-if (num % 2 === 0){
-console.log("O numero é par");
+// if (num % 2 === 0){
+// console.log("O numero é par");
 
-}else{
+// }else{
 
-console.log("O numero é impar")
-}
-
-
-//Exercicio 3
-
-let idade2 = prompt("Informe qual é sua idade");
+// console.log("O numero é impar")
+// }
 
 
-if (idade2 <= 12){
+// //Exercicio 3
 
-    console.log("Criança");
-  }else if (idade2 >= 13 && idade2 <= 17 ){
-        console.log("Adolescente");
-  }else   ( idade2 >=18); {
-    console.log("Adulto")
-  }
+// let idade2 = parseInt(prompt("Informe qual é sua idade"));
 
 
-  //Exercicio 4
+// if (idade2 <= 12){
 
-  let nota = prompt("Informe a nota do Aluno:");
-
-  if (nota >= 60){
-    console.log("Aprovado")
-  }else{
-    console.log("Reprovado")
-  }
-
-
-  //Exercicio 5 Calculadora
+//     console.log("Criança");
+//   }else if (idade2 >= 13 && idade2 < 18 ){
+//         console.log("Adolescente");
+//   }else  {
+//     console.log("Adulto")
+//   }
 
 
-let numero1 = parseInt(prompt("Informe o primeiro numero"));
-let numero2 = parseInt(prompt("Informe o segundo numero"));
+//   //Exercicio 4
+
+//   let nota = prompt("Informe a nota do Aluno:");
+
+//   if (nota >= 60){
+//     console.log("Aprovado")
+//   }else{
+//     console.log("Reprovado")
+//   }
 
 
-let operacao= prompt("Escolha uma operação");
-switch (operacao) {
+//   //Exercicio 1 Intermediario: Calculadora
 
-    case ("+"):
-    console.log(numero1 + numero2);
-        break;
 
-    case ("-"):
-    console.log(numero1 - numero2);
-        break;
+// let numero1 = parseInt(prompt("Informe o primeiro numero"));
+// let numero2 = parseInt(prompt("Informe o segundo numero"));
 
-    case ("*"):
-        console.log(numero1 * numero2);    
-        break;
 
-     case ("/"):
-        console.log(numero1 / numero2);
-        break;
+// let operacao= prompt("Escolha uma operação");
+// switch (operacao) {
 
-default:
-    console.log(erro);
-}
+//     case ("+"):
+//     console.log(numero1 + numero2);
+//         break;
 
-compra * 10/100
+//     case ("-"):
+//     console.log(numero1 - numero2);
+//         break;
+
+//     case ("*"):
+//         console.log(numero1 * numero2);    
+//         break;
+
+//      case ("/"):
+//         console.log(numero1 / numero2);
+//         break;
+
+// default:
+//     console.log(erro);
+// }
+
+// //Exercicio 2 Intermediario: 3 numeros com &&
+
+
+// let numero3 = parseInt(prompt("Insira numero 1"));
+// let numero4 = parseInt(prompt("Insira numero 2"));
+// let numero5 = parseInt(prompt("Insira numero 3"));
+
+// if (numero3 > numero4 && numero3 > numero5){
+//   console.log(numero3 + " é o maior numero")
+// }else if (numero4 > numero3 && numero4 > numero5){
+//   console.log(numero4 + "é o maior numero")
+// }else if (numero5 > numero3 && numero5 > numero4) {
+//   console.log(numero5 +" é o maior numero")
+// }else{
+//   console.log("Por favor digite numeros diferntes!")}
+
+
+
+//   //Exercicio 2 Intermediario: 3 numeros sem &&
+
+
+
+
+// //Exercicio 3 Intermediario: desconto 10%
+
+
+
+// let compra = parseInt(prompt("insira o valor da compra"));
+
+
+// if (compra > 100){
+//   console.log("10% de desconto : " + (compra - compra  * 0.1))
+// } else {
+//   console.log("O valor da compra é " +compra)
+// }
+
+
+
+// //Exercicio 4 Intermediario: login simples
+
+
+
+//Exercicio 1 Avançado
+
+// let lado1 = parseInt(prompt("Informe o lado a"));
+// let lado2 = parseInt(prompt("Infome o lado b"));
+// let lado3 = parseInt(prompt("Informe o lado c"));
+
+// if (lado1 == lado2 &&  lado1 == lado3){
+//   console.log("Triangulo equilatero")
+
+// }else if (lado1 != lado2 && lado2 !=lado3 && lado1 != lado3)
+//   console.log("Triangulo escaleno")
+
+// else {
+//   console.log("Triangulo isóceles")
+// }
+
+
+
+// //Exercicio 2 Avançado
+
+// let nota1 = parseInt(prompt("Informe sua nota"))
+
+// if (nota1 < 60 ){
+//   console.log("E")
+
+// } else if (nota1 < 90 && nota1 > 79){
+// console.log("B")
+
+// }else if (nota1 <80 && nota1 >69){
+//   console.log("C")
+
+// }else if(nota1 >59 && nota1 <70){
+// console.log("D")
+
+// }else {
+//   console.log("A")
+// }
