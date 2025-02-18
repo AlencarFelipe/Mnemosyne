abd29f164aafcdd2d3951aaf303c389e7e43b0a1
AlencarFelipe <alencarfelkipe@gmail.com>
2025-02-17 21:55:24 -0300

    Estrutura de repetição

---
+++ sandbox/repeticoes.js
@@ -0,0 +1,39 @@
+//Exercicio 1 FOR
+// for (let i = 1;i <=10; i++){
+//     console.log("Contagem:" , i);
+// }
+
+// //Exercicio 1 WHILE
+// let contador = 1;
+// while(contador <=10){
+
+//     console.log("Contagem:" , contador);
+//     contador++;
+// }
+
+//Exercicio 1Do while 
+// let contador = 1;
+//  do{
+//     console.log("Contador" , contador);
+//     contador++;
+//  } while (contador <=10);
+
+// //Exercicio 2 tabuada
+// let num = prompt("Insira um número: ");
+
+// for (let contador = 1; contador=10; contador++){
+
+//    let valorCalculado = num * contador;
+//    let mensagem = `${num} x ${contador} = ${valorCalculado}`
+// }
+
+//Exercicio 3 soma dos primeiros numeros
+// let num = parseInt(prompt("Insira um numero N"));
+ 
+// for (let contador = 1; contador <=num; contador++){
+// let valorCalculado = contador + num;
+// let mensagem= `${num} + ${contador} = ${valorCalculado}`
+// console.log(mensagem);
+// }
+
+
